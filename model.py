735748ff7d5105b3d2e8b0a726f73494ace13f28
ZiruiDeng <46855946+ZiruiDeng@users.noreply.github.com>
2019-06-05 22:49:46 +0800

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 class Network:
     def __init__(self):
         self.learning_rate = 0.001
-        # 记录已经训练的次数
         self.global_step = tf.Variable(0, trainable=False)
 
         self.x = tf.placeholder(tf.float32, [None, 784])
@@ -16,6 +15,8 @@ class Network:
 
         self.loss = -tf.reduce_sum(self.label * tf.log(self.y + 1e-10))
 
+        # minimize 可传入参数 global_step， 每次训练 global_step的值会增加1
+        # 因此，可以通过计算self.global_step这个张量的值，知道当前训练了多少步
         self.train = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(
             self.loss, global_step=self.global_step)
 
